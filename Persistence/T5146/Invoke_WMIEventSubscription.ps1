a4f363a1e149322d887183108bdbc03d4aa0d4d0
tomc0sm <76937076+tomc0sm@users.noreply.github.com>
2024-04-18 08:31:48 +0200

    Add files via upload

---
+++ Persistence/T5146/Invoke_WMIEventSubscription.ps1
@@ -0,0 +1,77 @@
+function Invoke-WMIEventSubscription {
+    <#
+    .SYNOPSIS
+
+    MITREATT&CK :https://attack.mitre.org/techniques/T1546/003/
+
+    .DESCRIPTION
+
+    MITREATT&CK : https://attack.mitre.org/techniques/T1546/003/
+
+    .PARAMETER OutFile
+
+    Export result to csv file. It can be absolute or relative path.
+
+    .PARAMETER Show
+
+    Output result
+
+
+    .EXAMPLE 
+
+    Invoke-WMIEventSubscription -Show
+    Invoke-WMIEventSubscription -OutFile .\T5146-WMIEventSubscription.csv -Show
+
+    #>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $False )]
+        [string]$OutFile,
+        [Parameter(Mandatory = $False )]
+        [switch]$Show
+    )
+
+    Import-Module -Name ($PSScriptRoot + "\..\..\Utils\Invoke-Utils.psd1") -Force
+
+    # Main
+    $ResultList = New-Object System.Collections.Generic.List[System.Object]
+    $EventList = New-Object System.Collections.Generic.List[System.Object]
+
+    $ExpectedProperties = "Name","__RELPATH", "PSComputerName" ,"__CLASS", "__SUPERCLASS" , "__NAMESPACE" , "__PATH", "Consumer", "Filter", "CommandLineTemplate" , "ExecutablePath", "Query"
+    
+    Get-WMIObject -Namespace root\Subscription -Class __EventFilter | ForEach-Object {
+        $EventList.Add($_)
+    }
+  
+    Get-WMIObject -Namespace root\Subscription -Class __EventConsumer | ForEach-Object {
+        $EventList.Add($_)
+    }
+
+    Get-WMIObject -Namespace root\Subscription -Class __FilterToConsumerBinding  | ForEach-Object {
+        $EventList.Add($_)
+    }
+
+
+    for($i =0; $i -lt $EventList.Count; $i++){
+        $WMIEvent = [PSCustomObject]@{}
+        foreach ($prop in $ExpectedProperties){
+            $WMIEvent | Add-Member -MemberType NoteProperty -Name $prop -Value  ($EventList[$i].$prop -join ",")
+        }
+        $ResultList.Add($WMIEvent) 
+    }
+   
+
+    # Outputs
+    if($PSBoundParameters.ContainsKey('OutFile') -eq $true){
+        $ResultList | Select-Object $sortedProperties | Export-Csv -Path $OutFile -NoTypeInformation -Encoding UTF8
+    }
+
+    if($PSBoundParameters.ContainsKey('Show') -eq $true){
+        Write-Output  $ResultList | Select-Object $sortedProperties | Format-List
+    }
+
+    return $ResultList
+
+}
+
