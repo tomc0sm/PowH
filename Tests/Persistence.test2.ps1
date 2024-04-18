8ff759d94bf64a3a6b1cb6f23e3835bebff6e067
tomc0sm <76937076+tomc0sm@users.noreply.github.com>
2024-04-18 08:25:41 +0200

    Update Persistence.test2.ps1

--- Tests/Persistence.test2.ps1
+++ Tests/Persistence.test2.ps1
@@ -1,1 +1,18 @@
+Import-Module -Name ($PSScriptRoot + "\..\Persistence\Persistence.psd1") -Force
 
+
+Invoke-ScheduledTask -OutFile ".\Output\Persistence\T5103_ScheduledTask.csv" -Show
+
+Invoke-RegistryRunKey -OutFile ".\Output\Persistence\T5147_RegistryRunKey.csv" -Show
+
+Invoke-StartUpFolder -OutFile ".\Output\Persistence\T5147_StartUpFolder.csv" -Show
+
+Invoke-WinLogon -OutFile ".\Output\Persistence\T5147_WinLogon.csv" -Show
+
+Invoke-Service -OutFile ".\Output\Persistence\T5143_Service.csv" -Show
+
+Invoke-AppInitDLL -OutFile ".\Output\Persistence\T5146_AppInitDLL.csv" -Show
+
+Invoke-WMIEventSubscription -OutFile ".\Output\Persistence\T5146-WMIEventSubscription.csv" -Show
+
+Invoke-BitsJobs -OutFile ".\Output\Persistence\T1197-BitsJobs.csv" -Show
